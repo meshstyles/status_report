64b298f74d0de524f8119d5080ac59eafd9d3fe4
meshstyles <meshstyles@users.noreply.github.com>
2020-08-19 22:17:10 +0200

    finalized tracking dhl
    - dhl ecommerce
    - dhl parcel de

--- run.sh
+++ run.sh
@@ -5,12 +5,32 @@ intro(){
     echo "# please make sure to run this script in it's own folder #"
     echo "#    confirm by writing something on the command line    #"
     echo "##########################################################"
+    read
     echo "if you found this please keep it so you don't have to see the intro again" > confirm.log
 }
 
+printpaket(){
+    echo ""
+    echo "Tracked package: ${traceid} - via ${service}"
+    echo "last status: ${statusCode} - ${status}"
+    echo "time: ${timestamp}"
+    [ "${location}" = "null" ] || echo "location: ${location}"
+    echo ""
+}
+
 dhlasia(){
-    package=$(curl -X GET "https://api-eu.dhl.com/track/shipments?trackingNumber=$traceid&service=ecommerce&language=en&limit=25" -H "accept: application/json" -H "DHL-API-Key: demo-key")
-    echo $package| jq '.'
+    sleep 5
+    package=$(curl -s -X GET "https://api-eu.dhl.com/track/shipments?trackingNumber=$traceid&service=ecommerce&language=en" -H "accept: application/json" -H "DHL-API-Key: demo-key")
+
+    eventnumber=$(echo $package | jq '.shipments[0].events | length')
+    eventnumber=$(expr $eventnumber - 1 )
+    event=$(echo $package | jq --arg index "$eventnumber" '.shipments[0].events[$index|tonumber]')
+    status=$(echo $event | jq -r '.status')
+    statusCode=$(echo $event | jq -r '.statusCode')
+    timestamp=$(echo $event | jq -r '.timestamp')
+    location=$(echo $event | jq -r '.location.address.addressLocality') 
+    service="DHL eCommerce Asia"
+    printpaket
 }
 
 trackasia(){
@@ -21,6 +41,30 @@ trackasia(){
     done <"$fileasiatrack"
 }
 
-[ -f confirm.log ] || intro
-[ -f asia.track ] && trackasia 
+dhlde(){
+    sleep 5
+    package=$(curl -s -X GET "https://api-eu.dhl.com/track/shipments?trackingNumber=$traceid&recipientPostalCode=$zip&service=parcel-de&language=en" -H "accept: application/json" -H "DHL-API-Key: demo-key")
 
+    event=$(echo $package | jq '.shipments[0].events[0]')
+    status=$(echo $event | jq -r '.status')
+    statusCode=$(echo $event | jq -r '.statusCode')
+    timestamp=$(echo $event | jq -r '.timestamp')
+    location=$(echo $event | jq -r '.location.address.addressLocality')
+    service="DHL PAKET DE"
+    printpaket
+}
+
+trackde(){
+    filedetrack="./de.track"
+        while IFS= read -r line
+            do  
+                traceid=$(echo "${line% *}")
+                zip=$(echo "${line#* }")
+                dhlde
+    done <"$filedetrack"
+}
+
+[ -f confirm.log ] || intro
+[ -f de.track ] && trackde
+[ -f asia.track ] && trackasia
+wget "https://opendata.dwd.de/weather/webcam/Offenbach-W/Offenbach-W_latest_full.jpg"
