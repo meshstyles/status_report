46b64fea274afc029dc1f5be4aa1b52128fe6a21
meshstyles <meshstyles@users.noreply.github.com>
2020-08-20 03:34:04 +0200

    added openweather

--- README.md
+++ README.md
@@ -15,8 +15,19 @@ reporting in the status of things.
 - run run.sh
 
 ## info
-this uses the DHL api, please substitue "demo-key" with your own api key
+this uses the DHL api, please substitute "key" with your own api key
+this uses openweather, please substitute "key" with your own api key
 
 ## dependencies
 - jq
 - curl
+
+## settings
+samples are provided
+- asia.track
+  - add the barcode (tracking number)
+- de.track
+  - add the barcode (tracking number) than a space followed by plz (german zipcode like XXXXX)
+- settings.json
+  - please replace key with your api key
+  - please replace the location with you location so you don't get weather for london
--- run.sh
+++ run.sh
@@ -9,6 +9,12 @@ intro(){
     echo "if you found this please keep it so you don't have to see the intro again" > confirm.log
 }
 
+setenv(){
+    dhlkey=$(cat settings.json | jq -r '.dhl')
+    openweatherkey=$(cat settings.json | jq -r '.openWeather')
+    weatherloc=$(cat settings.json| jq -r '.weatherLoc')
+}
+
 printpaket(){
     echo ""
     echo "Tracked package: ${traceid} - via ${service}"
@@ -19,9 +25,9 @@ printpaket(){
 }
 
 dhlasia(){
+    #use sleep here when you use demokey
     sleep 5
-    package=$(curl -s -X GET "https://api-eu.dhl.com/track/shipments?trackingNumber=$traceid&service=ecommerce&language=en" -H "accept: application/json" -H "DHL-API-Key: demo-key")
-
+    package=$(curl -s -X GET "https://api-eu.dhl.com/track/shipments?trackingNumber=$traceid&service=ecommerce&language=en" -H "accept: application/json" -H "DHL-API-Key: ${dhlkey}")
     eventnumber=$(echo $package | jq '.shipments[0].events | length')
     eventnumber=$(expr $eventnumber - 1 )
     event=$(echo $package | jq --arg index "$eventnumber" '.shipments[0].events[$index|tonumber]')
@@ -42,9 +48,9 @@ trackasia(){
 }
 
 dhlde(){
+    #use sleep here when you use demokey
     sleep 5
-    package=$(curl -s -X GET "https://api-eu.dhl.com/track/shipments?trackingNumber=$traceid&recipientPostalCode=$zip&service=parcel-de&language=en" -H "accept: application/json" -H "DHL-API-Key: demo-key")
-
+    package=$(curl -s -X GET "https://api-eu.dhl.com/track/shipments?trackingNumber=$traceid&recipientPostalCode=$zip&service=parcel-de&language=en" -H "accept: application/json" -H "DHL-API-Key: ${dhlkey}") 
     event=$(echo $package | jq '.shipments[0].events[0]')
     status=$(echo $event | jq -r '.status')
     statusCode=$(echo $event | jq -r '.statusCode')
@@ -64,6 +70,31 @@ trackde(){
     done <"$filedetrack"
 }
 
+toCelcius(){
+    tmp=$(echo "${1%.*}")
+    tmp=$(expr $tmp - 273)
+}
+
+openweather(){
+    weather=$(curl -s "http://api.openweathermap.org/data/2.5/weather?q=$weatherloc&APPID=$openweatherkey")
+    temp=$(echo $weather | jq -r '.main.temp')
+    toCelcius $temp
+    temp=$tmp
+    mintemp=$(echo $weather | jq -r '.main.temp_min')
+    toCelcius $mintemp
+    mintemp=$tmp
+    maxtemp=$(echo $weather | jq -r '.main.temp_max')
+    toCelcius $maxtemp
+    maxtemp=$tmp
+    humidity=$(echo $weather | jq -r '.main.humidity')
+    cloudiness=$(echo $weather | jq -r '.weather[0].description')
+    echo "temperature ${temp}C (min: ${mintemp}C | max: ${maxtemp}C )"
+    echo "humidity: ${humidity}%"
+    echo "weather: ${cloudiness}"
+}
+
 [ -f confirm.log ] || intro
+[ -f settings.json ] && setenv
 [ -f de.track ] && trackde
 [ -f asia.track ] && trackasia
+openweather
